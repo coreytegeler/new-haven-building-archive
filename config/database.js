090749cff0b248aa191f9e6f41b35c4b0d8218b1
coreytegeler <coreytegeler@gmail.com>
2016-07-24 16:05:28 -0400

    dbs

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-	'url': 'localhost:27017/new-haven-building-archive'
+  production: 'mongodb://heroku_9lc8j8h8:TY5sKAFZT-3ijkBOj2hn8luf5Y9@ds029735.mlab.com:29735/heroku_9lc8j8h8',
+  development: 'localhost:27017/new-haven-building-archive',
+  test: 'localhost:27017/new-haven-building-archive'
 };
