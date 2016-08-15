7855e888d61ff4925e756dc0adda19653e296b01
coreytegeler <coreytegeler@gmail.com>
2016-08-14 21:41:48 -0400

    mongo lab

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  production: 'mongodb://heroku_9lc8j8h8:TY5sKAFZT-3ijkBOj2hn8luf5Y9@ds029735.mlab.com:29735/heroku_9lc8j8h8',
+  production: process.env.MONGODB_URI,
   development: 'localhost:27017/new-haven-building-archive',
   test: 'localhost:27017/new-haven-building-archive'
 };
